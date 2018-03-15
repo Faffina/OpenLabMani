7a7d760d7743c4d2582ef73d0478d937dbcf6d76
faffa <esdpop@gmail.com>
2018-03-15 20:15:49 +0100

    nuove funzioni

--- sketch_mar06a.ino
+++ sketch_mar06a.ino
@@ -1,18 +1,10 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(6, 7, 2, 3, 4, 5);
-int ai, af, bi, bf;
-bool a = false, b = false;
+int ai, bi, ci, di, af, bf, cf, df;
+bool a = false, b = false, c = false, d = false;
+int modalita;
 
-void setup() {  
-  lcd.begin(16, 2);
-  pinMode(A0, INPUT); //a i
-  pinMode(A1, INPUT); //a f
-  pinMode(A2, INPUT); //b i
-  pinMode(A3, INPUT); //b f
- 
-}  
-  
-void loop(){
+void dueConometri(){
   if(!a){
     if(digitalRead(A0)){
       ai = millis();
@@ -21,25 +13,156 @@ void loop(){
   }
   else{
     if(digitalRead(A1)){
-      af = millis();
+      bi = millis();
       a = false;
-      lcd.setCursor(0, 0);
-      lcd.print(float(af - ai)/1000);
+      lcd.setCursor(3, 0);
+      lcd.print(float(bi - ai)/1000);
     }
   }
 
   if(!b){
     if(digitalRead(A2)){
-      bi = millis();
+      ci = millis();
       b =  true;
     }
   }
   else{
     if(digitalRead(A3)){
-      bf = millis();
+      di = millis();
       b = false;
-      lcd.setCursor(0, 1);
+      lcd.setCursor(3, 1);
+      lcd.print(float(di - ci)/1000);
+    }
+  }
+}
+
+void unConometro(){
+  if(!a){
+    if(digitalRead(A0)){
+      ai = millis();
+      a =  true;
+    }
+  }
+  else{
+    if(digitalRead(A1)){
+      bi = millis();
+      a = false;
+      lcd.setCursor(3, 0);
+      lcd.print(float(bi - ai)/1000);
+    }
+  }
+}
+
+void oscuramento(){
+  if(a)
+  {
+    if(!digitalRead(A0)){
+      af = millis();
+      lcd.setCursor(3, 0);
+      lcd.print(float(af - ai)/1000);
+      a = false;
+    }
+  }
+  else
+  {
+    if(digitalRead(A0))
+    {
+      ai = millis();
+      a = true;
+    }
+  }
+  if(b)
+  {
+    if(!digitalRead(A1)){
+      bf = millis();
+      lcd.setCursor(3, 1);
       lcd.print(float(bf - bi)/1000);
+      b = false;
+    }
+  }
+  else
+  {
+    if(digitalRead(A1))
+    {
+      bi = millis();
+      b = true;
+    }
+  }
+  if(c)
+  {
+    if(!digitalRead(A2)){
+      cf = millis();
+      lcd.setCursor(3, 2);
+      lcd.print(float(cf - ci)/1000);
+      c = false;
+    }
+  }
+  else
+  {
+    if(digitalRead(A2))
+    {
+      ci = millis();
+      c = true;
+    }
+  }
+  if(d)
+  {
+    if(!digitalRead(A3)){
+      df = millis();
+      lcd.setCursor(3, 4);
+      lcd.print(float(df - di)/1000);
+      d = false;
+    }
+  }
+  else
+  {
+    if(digitalRead(A3))
+    {
+      di = millis();
+      d = true;
+    }
+  }
+}
+
+void print(a, b, c, d){
+  lcd.setCursor(0,0);
+  lcd.print(a);
+  lcd.setCursor(0,1);
+  lcd.print(b);
+  lcd.setCursor(0,2);
+  lcd.print(c);
+  lcd.setCursor(0,3);
+  lcd.print(d);
+}
+
+void setup() {  
+  lcd.begin(16, 2);
+  pinMode(A0, INPUT); //a i
+  pinMode(A1, INPUT); //a f
+  pinMode(A2, INPUT); //b i
+  pinMode(A3, INPUT); //b f
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+  digitalWrite(8, HIGH);
+}  
+  
+void loop(){
+  if(digitalRead(8)){
+    menu();
+  }
+  else{
+    switch(modalita){
+      case 0:
+        print("ab ", "", "", "");
+        unConometro();
+        break;
+      case 1:
+        print("ab", "cd", "", "");
+        dueConometri();
+        break;
+      case 2:
+        print("a", "b", "c", "d")
+        break;
     }
   }
 }  
