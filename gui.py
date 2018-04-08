32cd962b9ab37a73a5ed5d518f85fa405e1d3668
faffa <esdpop@gmail.com>
2018-04-08 14:29:44 +0200

    faffa

--- gui.py
+++ gui.py
@@ -1,5 +1,25 @@
-# !/usr/bin/python3
+#!/usr/bin/env python3
+import time
 from tkinter import *
-from tkinter import messagebox
+import serial
 
-data = {"a":[],"b":[],"c":[], "d":[]}
+ser = serial.Serial('/dev/ttyACM0', 9600) 
+data = {"a":[], "b":[],"c":[],"d":[]}
+
+def serDectpit():
+    mess = ser.readline().decode("utf-8")
+    mylist.insert(END,mess)
+
+root = Tk()
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill = Y )
+
+mylist = Listbox(root, yscrollcommand = scrollbar.set )
+
+mylist.pack( side = LEFT, fill = BOTH )
+scrollbar.config( command = mylist.yview )
+
+while True:
+    serDectpit()
+    root.update_idletasks()
+    root.update()
